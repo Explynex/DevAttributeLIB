8a6cad9455a60b73c6637f8e70c0ac4e8dc0dce8
Explyne <k.egazarov2004@gmail.com>
2022-05-16 13:53:26 +0300

    upd 1.4 reloaded - removed str tools class - add templates alloc and free mem for two-dimensional arrays - some small fixes - is many funcs add support UNICODE and MULTIBYTE mode - removed one pause function

--- devattribute/devattribute.cpp
+++ devattribute/devattribute.cpp
@@ -1,18 +1,14 @@
 ﻿#include "devattribute.h"
 #include <iostream>
 
+
 static HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
 static HANDLE hStdIn = GetStdHandle(STD_INPUT_HANDLE);
-CONSOLE_SCREEN_BUFFER_INFO scrBufferInfo;
-
-void winPause()
-{
-	system("pause > nul");
-}
+static CONSOLE_SCREEN_BUFFER_INFO scrBufferInfo;
 
 void pause()
 {
-	_getch();
+	system("pause > nul");
 }
 
 void setColor(consoleColor foreground, consoleColor background)
@@ -62,7 +58,10 @@ void GotoXY(short X, short Y, std::string print, consoleColor foreground, consol
 	COORD cord = { X, Y };
 	SetConsoleCursorPosition(hStdOut, cord);
 	if (foreground != WHITE || background != BLACK) setColor(foreground, background);
-	if (print != "") std::cout << print;
+	if (print != "") {
+		std::cin.clear();
+		std::cout << print;
+	}
 	if (foreground != WHITE || background != BLACK) setColor(WHITE, BLACK);
 }
 
@@ -77,7 +76,6 @@ void sheetGenerator(int x, int y, const int sizeX, const int sizeY, int lengthX,
 		{ "█","▄","█","█","█","▄","█","█","▄","█","▄" },
 		{ "█","█","█","█","█","█","█","█","█","█","█" }
 	};
-	utf.setUtfLocale();
 	lengthY += 1;
 	int buffsizeY = sizeY * lengthY + 1, buffX = x;
 	setColor(text, background);
@@ -121,32 +119,31 @@ void cleaning(int x, int y, int width, int height, consoleColor background)
 	for (int i = 0; i < height; i++) {
 		GotoXY(x, buff++);
 		for (int j = 0; j < width; j++)
-			printf(" ");
+			std::cout << " ";
 	}
 	setColor(WHITE, BLACK);
 	GotoXY(x, y);
 }
 
-void setConsoleSize(int columnsX, int linesY) 
+void setConsoleSize(int columnsX, int linesY)
 {
 	std::string command = "mode con cols=" + std::to_string(columnsX) + " lines=" + std::to_string(linesY);
 	system(command.c_str());
 }
 
-int getConsoleSize(std::string position) 
+void getConsoleSize(COORD &position) 
 {
 	GetConsoleScreenBufferInfo(hStdOut, &scrBufferInfo);
 	short winWidth = scrBufferInfo.srWindow.Right - scrBufferInfo.srWindow.Left + 1;
 	short winHeight = scrBufferInfo.srWindow.Bottom - scrBufferInfo.srWindow.Top + 1;
-	if (position == "width") return winWidth;
-	if (position == "height") return winHeight;
-	return 0;
+	position.X = winWidth;
+	position.Y = winHeight;
 }
 
 void setConsoleFullscreen() 
 {
 	COORD newSize;
-	short bufferHeight = getConsoleSize("height");
+	short bufferHeight = scrBufferInfo.dwSize.Y;
 	short bufferWidth = scrBufferInfo.dwSize.X;
 	SetConsoleDisplayMode(GetStdHandle(STD_OUTPUT_HANDLE), CONSOLE_FULLSCREEN_MODE, &newSize);
 	keybd_event(VK_MENU, 0x38, 0, 0);
@@ -304,9 +301,9 @@ void setConsoleNoSelection(BOOL status)
 
 COORD setConsoleButton(
 	int x, int y, int width, int height, int countButtonsX, int countButtonsY,
-	int returnButton, consoleColor inactiveButton, consoleColor activeButton, int YCorrect, bool boarder,
-	int boarder_style, consoleColor boarderColor,consoleColor backBoarderColor,
-	std::string titles[], int titlesIndent, consoleColor titlesColor
+	int returnButton, consoleColor inactiveButton, consoleColor activeButton,
+	int YCorrect, int XCorrect, bool boarder, int boarder_style,
+	consoleColor boarderColor, consoleColor backBoarderColor, std::string titles[], consoleColor titlesColor
 )
 {
 	RECT r{};
@@ -315,79 +312,81 @@ COORD setConsoleButton(
 	HWND wh = GetConsoleWindow();
 	showConsoleCursor(FALSE);
 	for (int i = 0; i < countButtonsY; i++) buttonCord[i] = new COORD[countButtonsX];
-	if (boarder) sheetGenerator(x - 1, y - 1, countButtonsX, countButtonsY, width, height,boarder_style,boarderColor,backBoarderColor);
-		for (int i = 0, tempY = y ; i < countButtonsY; i++) {
-			for (int j = 0, tempX = x; j < countButtonsX; j++) {
-				if (inactiveButton != -1 || activeButton != -1) 
-					cleaning(tempX, tempY, width, height, inactiveButton);
-					buttonCord[i][j].X = tempX;
-					buttonCord[i][j].Y = tempY;
-				if (titles != 0 && countButtonsX == 1) {
-					GotoXY(tempX + titlesIndent,tempY+(height/2), titles[i], titlesColor, inactiveButton);
-				}
-				else if (titles != 0 && countButtonsY == 1) {
-					GotoXY(tempX + titlesIndent, tempY + (height / 2), titles[j], titlesColor, inactiveButton);
-				}
-
-				if (!boarder) tempX += width + 2;
-				else tempX += width + 1;
+	if (boarder) sheetGenerator(x - 1, y - 1, countButtonsX, countButtonsY, width, height, boarder_style, boarderColor, backBoarderColor);
+	for (int i = 0, tempY = y; i < countButtonsY; i++) {
+		for (int j = 0, tempX = x; j < countButtonsX; j++) {
+			if (inactiveButton != -1 || activeButton != -1)
+				cleaning(tempX, tempY, width, height, inactiveButton);
+			buttonCord[i][j].X = tempX;
+			buttonCord[i][j].Y = tempY;
+			if (titles != 0 && countButtonsX == 1) {
+				GotoXY(tempX, tempY + (height / 2), titles[i], titlesColor, inactiveButton);
+			}
+			else if (titles != 0 && countButtonsY == 1) {
+				GotoXY(tempX, tempY + (height / 2), titles[j], titlesColor, inactiveButton);
 			}
 
-			tempY += height + 1;
+			if (!boarder) tempX += width + 2;
+			else tempX += width + 1;
 		}
+
+		tempY += height + 1;
+	}
 	int posX = 0, posY = 0, buttonActive = 0, i = 0, j = 0, tempCordX = 0, tempCordY = 0,
-		mCord = 0,buttonPointX = 0, buttonPointY = 0; // переменные для проверки находится или нет курсор на конкретной кнопке
+		mCord = 0, buttonPointX = 0, buttonPointY = 0; // переменные для проверки находится или нет курсор на конкретной кнопке
 	while (true) {
 		GetWindowRect(wh, &r);
 		GetCursorPos(&p);
-		posX = ((p.x - r.left) / 8);
-		posY = ((p.y - r.top) / 16)-YCorrect;
+		posX = ((p.x - r.left) / (8 - XCorrect));
+		posY = ((p.y - r.top) / (16 - YCorrect));
 		for (i = 0; i < countButtonsY; i++) {
 			for (j = 0; j < countButtonsX; j++) {
 				if (countButtonsX <= countButtonsY) mCord = i;
 				else mCord = j;
-					if (posX >= buttonCord[i][j].X && posX <= buttonCord[i][j].X + width && posY >= buttonCord[i][j].Y && posY <= buttonCord[i][j].Y + height) {
-						if (tempCordX != buttonCord[i][j].X || tempCordY != buttonCord[i][j].Y) {
+				if (posX >= buttonCord[i][j].X && posX <= buttonCord[i][j].X + width && posY >= buttonCord[i][j].Y && posY <= buttonCord[i][j].Y + height) {
+					if (tempCordX != buttonCord[i][j].X || tempCordY != buttonCord[i][j].Y) {
+						if (inactiveButton != NUL && activeButton != NUL) {
 							cleaning(buttonCord[i][j].X, buttonCord[i][j].Y, width, height, activeButton);
 							if (titles != 0)
-								GotoXY(buttonCord[i][j].X + titlesIndent,buttonCord[i][j].Y + (height / 2), titles[mCord], titlesColor, activeButton);
+								GotoXY(buttonCord[i][j].X, buttonCord[i][j].Y + (height / 2), titles[mCord], titlesColor, activeButton);
 							if (tempCordX != 0 || tempCordY != 0) {
 								cleaning(tempCordX, tempCordY, width, height, inactiveButton);
 								if (titles != 0)
-									GotoXY(tempCordX + titlesIndent, tempCordY + (height / 2), titles[mCord+1], titlesColor, inactiveButton);
+									GotoXY(tempCordX, tempCordY + (height / 2), titles[mCord + 1], titlesColor, inactiveButton);
 							}
-							tempCordX = buttonCord[i][j].X;
-							tempCordY = buttonCord[i][j].Y;
+						}
+						tempCordX = buttonCord[i][j].X;
+						tempCordY = buttonCord[i][j].Y;
 
-						}
-						buttonPointY = i; buttonPointX = j;
-						if (GetKeyState(returnButton) < 0) {
-							for (int i = 0; i < countButtonsY; i++) // очистка памяти под массив координат кнопок
-								delete[] buttonCord[i];
-							delete[] buttonCord;
-							if (countButtonsY != 1 || countButtonsX != 1) {
-								numButton.X = j+1;
-								numButton.Y = i+1;
-							}
-							else {
-								numButton.X = 1;
-								numButton.Y = 1;
-							}
-							return numButton;
-						}
 					}
-					else if (i == buttonPointY && buttonPointX == j && (tempCordX != 0 || tempCordY != 0)) {
-						cleaning(tempCordX, tempCordY, width, height, inactiveButton);
-						if (titles != 0) 
-							GotoXY(tempCordX + titlesIndent,tempCordY + (height / 2), titles[mCord], titlesColor, inactiveButton);
-						tempCordX = 0;
-						tempCordY = 0;
+					buttonPointY = i; buttonPointX = j;
+					if (GetKeyState(returnButton) < 0) {
+						for (int i = 0; i < countButtonsY; i++) // очистка памяти под массив координат кнопок
+							delete[] buttonCord[i];
+						delete[] buttonCord;
+						if (countButtonsY != 1 || countButtonsX != 1) {
+							numButton.X = j + 1;
+							numButton.Y = i + 1;
+						}
+						else {
+							numButton.X = 1;
+							numButton.Y = 1;
+						}
+						return numButton;
 					}
+				}
+				else if (inactiveButton != NUL && i == buttonPointY && buttonPointX == j && (tempCordX != 0 || tempCordY != 0)) {
+					cleaning(tempCordX, tempCordY, width, height, inactiveButton);
+					if (titles != 0)
+						GotoXY(tempCordX, tempCordY + (height / 2), titles[mCord], titlesColor, inactiveButton);
+					tempCordX = 0;
+					tempCordY = 0;
+				}
 			}
 
 		}
 		if (GetKeyState(VK_ESCAPE) < 0) {
-			for (int i = 0; i < countButtonsY; i++) 
+			for (int i = 0; i < countButtonsY; i++)
 				delete[] buttonCord[i];
 			delete[] buttonCord;
 			numButton.X = 0;
@@ -398,60 +397,8 @@ COORD setConsoleButton(
 	}
 }
 
-std::string printFilter(int length, int x, int y, std::string mode, std::string min, std::string max, int minDig, int maxDig)
-{
-	if (mode.length() > 3 && min < "0" || mode.length() >3 && max > "9") {
-		min = "0"; max = "9";
-	}
-	std::wstring wmin = utf.stows(min), wmax = utf.stows(max);
-	int factive = 0;
-	std::wstring buff;
-	for (int i = 0; i <= length + 1; i++) {
-	m: std::wstring key;
-		key += _getwch();
-		if (i == length + 1) {
-			i--;
-			goto m;
-		}
-		if (GetAsyncKeyState(VK_DOWN) != 0 || GetAsyncKeyState(VK_UP) != 0 ||
-			GetAsyncKeyState(VK_LEFT) != 0 || GetAsyncKeyState(VK_RIGHT) != 0) goto m;
-		if (GetAsyncKeyState(VK_ESCAPE) != 0) return "0";
-		if (GetAsyncKeyState(VK_BACK) != 0) {
-			if (i != 0) {
-				if (i == factive)
-					factive = 0;
-				i--;
-				buff.erase(buff.length() - 1, 1);
-				GotoXY(x + i, y, utf.wstos(key));
-				GotoXY(x + i, y, " ");
-				GotoXY(x + i, y);
-			}
-			goto m;
-		}
-		if (GetAsyncKeyState(VK_RETURN) != 0 && i != 0) {
-			if (mode.length() > 3 && stof(buff) < minDig || mode.length() > 3 && stof(buff) > maxDig)
-				goto m;
-			return utf.wstos(buff);
-		}
-		if (mode == "str" && key < wmin || key > wmax || mode.length() > 4 && key < wmin || key > wmax)
-			goto m;
-		if (mode == "calc") {
-			if (key < wmin && key != L"-" && key != L"." || key > wmax && key != L"-" && key != L".")
-				goto m;
-			if (key == L"-" && i != 0) goto m;
-			if (key == L".") {
-				if (factive != 0 || i == 0) goto m;
-				factive = i;
-			}
-		}
-		if (i != length) {
-			buff += key;
-			GotoXY(x + i, y, utf.wstos(key));
-		}
-	}
-}
 
-/*void setConsoleTrayIcon(const wchar_t* trayInfo, LPCWSTR pathIcon, WNDPROC msgCallback, UINT flags)
+void setConsoleTrayIcon(const wchar_t* trayInfo, LPCWSTR pathIcon, WNDPROC msgCallback, UINT flags)
 {
 	HINSTANCE hinstance{};
 	NOTIFYICONDATA icon{};
@@ -466,9 +413,16 @@ std::string printFilter(int length, int x, int y, std::string mode, std::string 
 	icon.cbSize = sizeof(NOTIFYICONDATA);
 	icon.hWnd = windowHwnd;
 	icon.uCallbackMessage = WM_USER;
+#if defined(_UNICODE)
 	icon.hIcon = (HICON)LoadImage(NULL, pathIcon, IMAGE_ICON, 0, 0, LR_LOADFROMFILE | LR_SHARED);
-	icon.uFlags = flags;
 	wcscpy(icon.szTip, trayInfo);
+#else defined (_MBCS)
+	char* str{};
+	wcstombs(str, trayInfo, wcslen(trayInfo));
+	icon.hIcon = (HICON)LoadImageW(NULL, pathIcon, IMAGE_ICON, 0, 0, LR_LOADFROMFILE | LR_SHARED);
+	strcpy(icon.szTip, str);
+#endif
+	icon.uFlags = flags;
 	Shell_NotifyIcon(NIM_ADD, &icon);
 
 	MSG message;
@@ -477,22 +431,6 @@ std::string printFilter(int length, int x, int y, std::string mode, std::string 
 		DispatchMessage(&message);
 	}
 	Shell_NotifyIcon(NIM_DELETE, &icon);
-}*/
-
-std::string operator - (std::string str1, std::string str2)
-{
-	int len1 = str1.length(), len2 = str2.length();
-	for (int i = 0; i < len1; i++) {
-		if (str1[i] == str2[0]) {
-			for (int j = 1, m = i + 1; j < len2; ++m, ++j) {
-				if (str1[m] == str2[j] && j == len2 - 1) {
-					return str1.erase(i, len2);
-				}
-				else if (str1[m] != str2[j]) break;
-			}
-		}
-	}
-	return str1;
 }
 
 int fact(int val)
@@ -500,106 +438,6 @@ int fact(int val)
 	if (val ? 0 : 1) return 1;
 	return val *= fact(val - 1);
 }
-
-// STRTOOLS CLASS 
-
-std::string strtools::toupper(std::string str,std::string flag) 
-{
-	std::wstring wstr = stows(str);
-	if (flag == "") {
-		for (int i = 0; i < wstr.length(); i++) {
-			if (wstr[i] >= 'a' && wstr[i] <= 'z') {
-				wstr[i] = wstr[i] - 32;
-			}
-			else {
-				for (int j = 0; j < RUdown.length(); j++) {
-					if (wstr[i] == RUdown[j])
-						wstr[i] = RUup[j];
-					else {
-						for (int u = 0; u < UAdown.length(); u++) {
-							if (wstr[i] == UAdown[u])
-								wstr[i] = UAup[u];
-						}
-					}
-
-				}
-			}
-		}
-	}
-	if (flag == "el_GR") {
-		for (int i = 0; i < wstr.length(); i++) {
-			for (int j = 0; j < GRdown.length(); j++) {
-				if (wstr[i] == GRdown[j])
-					wstr[i] = GRup[j];
-			}
-		}
-	}
-	return str = wstos(wstr);
-}
-
-std::string strtools::tolower(std::string str, std::string flag) 
-{
-	std::wstring wstr = stows(str);
-	if (flag == "") {
-		for (int i = 0; i < wstr.length(); i++) {
-			if (wstr[i] >= 'A' && wstr[i] <= 'Z') {
-				wstr[i] = wstr[i] + 32;
-			}
-			else {
-				for (int j = 0; j < RUup.length(); j++) {
-					if (wstr[i] == RUup[j])
-						wstr[i] = RUdown[j];
-					else {
-						for (int u = 0; u < UAup.length(); u++) {
-							if (wstr[i] == UAup[u])
-								wstr[i] = UAdown[u];
-						}
-					}
-
-				}
-			}
-		}
-	}
-	if (flag == "el_GR") {
-		for (int i = 0; i < wstr.length(); i++) {
-			for (int j = 0; j < GRup.length(); j++) {
-				if (wstr[i] == GRup[j])
-					wstr[i] = GRdown[j];
-			}
-		}
-	}
-	return wstos(wstr);
-}
-
-std::string strtools::strexplus(std::string str1, std::string str2) 
-{
-	std::wstring wstr1 = utf.stows(str1), wstr2 = utf.stows(str2);
-	for (int i = wstr1.length() - 1; 0 <= i; i--)
-		for (int j = 0; j < wstr2.length(); j++)
-			if (wstr1[i] == wstr2[j])
-				wstr1.erase(i, 1);
-	return utf.wstos(wstr1);
-}
-
-int strtools::strfind(std::string str1, std::string str2) 
-{
-	int length1 = str1.length(), length2 = str2.length();
-	std::string strbuff;
-	for (int i = 0; i < length1; i++) {
-		for (int j = 0; j < length2; j++) {
-			if (length2 <= length1) {
-				if (str1[i] == str2[j] && str1[i + 1] == str2[j + 1] || length2 == 1)
-					strbuff += str2[j];
-				else if (i == length1 - 1 && j == length2 - 1)
-					strbuff += str2[j];
-			}
-		}
-		if (strbuff == str2) return 1;
-	}
-	return 0;
-}
-
-// END STRTOOLS CLASS 
 
 
 // MEMORY TOOLS
@@ -628,7 +466,7 @@ namespace memory
 		}
 		if (sysInfo == SystemMemoryListInformation) {
 			if (checkPrivilege(processToken, TEXT("SeProfileSingleProcessPrivilege"), TRUE)) {
-				SYSTEM_MEMORY_LIST_COMMAND command = command;
+				SYSTEM_MEMORY_LIST_COMMAND command{};
 				status = NtSetSystemInformation(sysInfo, &command, sizeof(command));
 				if (status >= 0) return 1;
 				else return 0;
