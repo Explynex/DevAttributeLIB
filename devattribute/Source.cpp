4fc97c0d20d198b09d6d9d62926aaa35be014943
Explyne <k.egazarov2004@gmail.com>
2022-05-16 13:53:26 +0300

    upd 1.4 - removed str tools class - add templates alloc and free mem for two-dimensional arrays - some small fixes - is many funcs add support UNICODE and MULTIBYTE mode - removed one pause function

--- devattribute/Source.cpp
+++ devattribute/Source.cpp
@@ -1,29 +1,74 @@
-﻿#include "devattribute.h"
-#include <iostream>
-#include <thread>
+﻿#include <iostream>
+#include <conio.h>
+#include <Windows.h>
+#include <codecvt>
+#include <string>
 
+class UnicodeString {
 
-
-
-
-/*posY -= y;
-posX -= x;
-
-if (posY % (height + 1) == 0) GotoXY(10, 1, "BORDER");*/
-
-
-int main() {
-
-	Sleep(100);
-	utf.setUtfLocale();
-	std::string titles[3] = { "Главное меню","Поебота","Выйти в санузел" };
-	COORD key = setConsoleButton(5, 1, 6, 3, 6, 6, VK_RBUTTON, BROWN, WHITE, 1, false, 0, NUL, NUL, titles, 3, BLACK);
-	if (key.X == 0 && key.Y == 0) {
-		std::cout << "Выйшло пойти нахуй";
-	}
+private:
+	wchar_t* wstr = nullptr;
+	std::wstring_convert<std::codecvt_utf8<wchar_t>> converter;
+	int size = 0;
+public:
 	
-	std::string  ass = "Выйти в главное меню";
-	GotoXY(1, 2, ass, YELLOW);
-	winPause();
+	friend std::ostream& operator << (std::ostream& out, UnicodeString& _str);
+	friend std::wostream& operator << (std::wostream& wout, UnicodeString& _str);
+
+	UnicodeString(const wchar_t* _str = L"")
+	{
+		size = wcslen(_str) + 1;
+		wstr = new wchar_t[size];
+		for (size_t i = 0; i < size; i++)
+			wstr[i] = _str[i];
+
+	}
+
+	bool operator > (UnicodeString& ustr) {
+		return length() > ustr.length();
+	}
+
+	int length() {
+		return wcslen(wstr);
+	}
+
+	operator std::string()
+	{
+		return converter.to_bytes(wstr);
+	}
+
+	~UnicodeString() {
+		delete[] wstr;
+	}
+};
+
+std::ostream& operator << (std::ostream& out, UnicodeString& ustr) {
+	out << (std::string)ustr;
+	return out;
 }
 
+std::wostream& operator << (std::wostream& wout, UnicodeString& ustr) {
+	wout << ustr.wstr;
+	return wout;
+}
+
+int UnicodeString::_compare(const String& rhs) const {
+
+	if (_str_len < rhs._str_len)
+		return 1;
+	else if (_str_len > rhs._str_len)
+		return -1;
+
+	return strcmp(_str, rhs._str);
+}
+
+int main() {
+	UnicodeString ustr = L"dick";
+	UnicodeString penis = L"sucks";
+	std::wstring ass = L"ASAS";
+	if (penis > ustr) {
+		std::cout << "bigger";
+	}
+
+	_getch();
+}
